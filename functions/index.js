107ca3231d59e8be15c4c36572352dfb78871c12
riricardo <ricardo.augusto.cardona@gmail.com>
2025-11-19 13:53:28 +0000

    adding ci cd

--- functions/index.js
+++ functions/index.js
@@ -4,5 +4,5 @@ import { initializeApp } from "firebase-admin/app";
 initializeApp();
 
 export const helloWorld = onRequest({ region: "europe-west2" }, (req, res) => {
-  res.send("Hello from functions");
+  res.send("Hello from functions with ci cd");
 });
